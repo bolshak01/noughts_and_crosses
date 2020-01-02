fe97644d26421dfc2e16404a374c367c44482be3
bolshak01 <alexeydurvin@gmail.com>
2020-01-02 14:25:34 +0300

    Create moves

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import {findField} from "../controllers/Mouse";
 
-export default function Board(props) {
+export default function Board() {
     const refBoard = React.useRef(null);
+
+    const [crosses, setCrosses] = React.useState([]);
+    const [noughts, setNoughts] = React.useState([]);
+    const [move, setMove] = React.useState('cross');
+
+    const revertMove = () => {
+        if (move === 'cross') {
+            setMove('nought')
+        } else {
+            setMove('cross')
+        }
+    };
+
     const drawLine = (context, start, end) => {
         context.moveTo(start.x, start.y);
         context.lineTo(end.x, end.y);
@@ -14,19 +27,40 @@ export default function Board(props) {
         drawLine(context, {x: 200, y: 0}, {x: 200, y: 300});
         drawLine(context, {x: 0, y: 100}, {x: 300, y: 100});
         drawLine(context, {x: 0, y: 200}, {x: 300, y: 200});
-        context.strokeStyle = 'gray';
-        context.stroke();
+    };
+
+    const drawNought = (context, center) => {
+        context.moveTo(center.x + 40, center.y);
+        context.arc(center.x, center.y, 40, 0, 2*Math.PI, false);
+    };
+
+    const drawCross = (context, center) => {
+        drawLine(context, {x: center.x - 40, y: center.y - 40}, {x: center.x + 40, y: center.y + 40});
+        drawLine(context, {x: center.x + 40, y: center.y - 40}, {x: center.x - 40, y: center.y + 40});
     };
 
     React.useEffect(() => {
         const context = refBoard.current.getContext('2d');
         context.beginPath();
         drawBoard(context);
+        context.strokeStyle = 'gray';
+        context.stroke();
     });
+
+    const setField = (field) => {
+        if (move === 'cross') {
+            crosses.push(field);
+            setCrosses(crosses);
+        } else {
+            noughts.push(field);
+            setNoughts(noughts);
+        }
+    };
 
     const mouseControl = (e) => {
         const field = findField({x: e.clientX - window.innerWidth/2 + 150, y: e.clientY});
-        alert(field);
+        setField(field);
+        revertMove();
     };
 
     return <canvas
