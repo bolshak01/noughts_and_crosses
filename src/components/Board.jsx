8851d29b004dc8807f35376760cf9f5ec116d632
bolshak01 <alexeydurvin@gmail.com>
2020-01-02 13:22:49 +0300

    Create board

---
+++ src/components/Board.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function Board(props) {
+    const refBoard = React.useRef(null);
+    const drawLine = (context, start, end) => {
+        context.moveTo(start.x, start.y);
+        context.lineTo(end.x, end.y);
+    };
+
+    const drawBoard = (context) => {
+        context.rect(0, 0, 300, 300);
+        drawLine(context, {x: 100, y: 0}, {x: 100, y: 300});
+        drawLine(context, {x: 200, y: 0}, {x: 200, y: 300});
+        drawLine(context, {x: 0, y: 100}, {x: 300, y: 100});
+        drawLine(context, {x: 0, y: 200}, {x: 300, y: 200});
+        context.strokeStyle = 'gray';
+        context.stroke();
+    };
+
+    React.useEffect(() => {
+        const context = refBoard.current.getContext('2d');
+        context.beginPath();
+        drawBoard(context);
+    });
+
+    return <canvas
+        ref={refBoard}
+        width='300px'
+        height='300px'
+        >
+        Your browser does not support canvas
+    </canvas>
+}
